c2dc24277c7ae70aa26adb411a3aa40d022278a5
mkovachev <mkovachev6@gmail.com>
2020-08-04 13:54:55 +0300

    added audit interface

--- Dashboard.Server/Dashboard.Data/Models/User.cs
+++ Dashboard.Server/Dashboard.Data/Models/User.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Dashboard.Data.Contracts;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 
 namespace Dashboard.Server.Data.Models
 {
-    public class User : IdentityUser
+    public class User : IdentityUser, IAuditable, IDeletable
     {
         public IEnumerable<Item> Items { get; } = new HashSet<Item>();
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+        public DateTime? DeletedOn { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
