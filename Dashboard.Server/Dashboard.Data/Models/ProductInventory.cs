d0a14e13f050c432c5f2590fe17a5d3696244ce0
mkovachev <mkovachev6@gmail.com>
2020-08-04 16:51:19 +0300

    created

---
+++ Dashboard.Server/Dashboard.Data/Models/ProductInventory.cs
@@ -0,0 +1,19 @@
+﻿using Dashboard.Data.Contracts;
+using System;
+
+namespace Dashboard.Data.Models
+{
+    public class ProductInventory : IAuditable, IDeletable
+    {
+        public int Id { get; set; }
+        public int CurrentQuantity { get; set; }
+
+        public int MinQuantity { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public DateTime? DeletedOn { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
